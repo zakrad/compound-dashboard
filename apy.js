ba3f7ab5fd27d96398b187b9b2efff20163a28c3
saxon <1@1.com>
2022-06-20 22:02:40 +0430

    Compound APY  1

--- apy.js
+++ apy.js
@@ -20,3 +20,46 @@ async function calculateSupplyApy(cToken) {
 
     return 100 * (Math.pow((supplyRatePerBlock / ethMentissa * blocksPerDay) + 1, daysPerYear - 1) - 1)
 }
+
+async function calculateCompApy(cToken, ticker, underlyingDecimals) {
+    let compSpeed = await Compound.eth.read(
+        comptroller,
+        'function compSpeed(address cToken) public view returns(uint)',
+        [cToken],
+        { provider }
+    );
+
+    let compPrice = await Compound.eth.read(
+        opf,
+        'function price(string memory symbol) external view returns(uint)',
+        [Compound.COMP],
+        { provider }
+    )
+
+    let underlyingPrice = await Compound.eth.read(
+        opf,
+        'function price(string memory symbol) external view returns(uint)',
+        [ticker],
+        { provider }
+    );
+
+    let totalSupply = await Compound.eth.read(
+        cToken,
+        'function totalSupply() public view returns(uint)',
+        [],
+        { provider }
+    );
+
+    let exchangeRate = await Compound.eth.read(
+        cToken,
+        'function exchangeRateCurrent() public view returns(uint)',
+        [],
+        { provider }
+    );
+
+    compSpeed = compSpeed / 1e18;
+    compPrice = compPrice / 1e6;
+    underlyingPrice = underlyingPrice / 1e6;
+    exchangeRate = +exchangeRate.toString() / ethMentissa;
+    
+}
